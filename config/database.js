88b63de87545e0f9661ff8b1ce08afe927ac10e1
riteshco <riteshgadwalv2@gmail.com>
2025-06-05 11:57:38 +0530

    Add(db.sql) command for creating database and Download dependencies
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>

--- config/database.js
+++ config/database.js
@@ -6,12 +6,12 @@ const dbConfig = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
-});
+  database: process.env.DB_NAME,
+}).promise();
 
 dbConfig.connect(function(err) {
   if (err) {
-    throw err;
+   throw err;
   }
   console.log('Connected to the database successfully');
 });
